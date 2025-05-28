32e1e3219fc400979d4bf040fb311d7659d3ed42
Erickhen1 <erickhen1@gmail.com>
2025-05-28 20:29:17 -0300

    Delete Freelancer Now directory

--- Freelancer Now/src/lib/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://vutjlolwnfxnnmjtmidr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1dGpsb2x3bmZ4bm5tanRtaWRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NjE5NTUsImV4cCI6MjA2NDAzNzk1NX0.6CgCO_PCSEo6YBhnetjS5IO0tFVpzynMf_uMAXVaDGA';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
